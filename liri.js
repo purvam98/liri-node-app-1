3d4519ff6610af643f1f1bc44fdadb2288d3f70d
Stefanie Ding <StefanieDing@Stefanie-Dings-MacBook-Pro.local>
2016-10-03 21:08:09 -0400

    create initial files

---
+++ liri.js
@@ -0,0 +1,28 @@
+//Grab data from keys.js
+//var twitterKey = data from keys.js
+
+var command = process.argv[2];
+//either song or movie
+var x = process.argv[3];
+
+if(command == 'my-tweets'){
+  //Display last 20 Tweets
+}
+
+if(command == 'spotify-this-song'){
+  //var print = x !? ('"The Sign" by Ace of Base') 
+  //else Display artist(s), song name, preview link of song from Spotify, album
+}
+
+if(command == 'movie-this'){
+  //if doesn't exist
+  //If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+  //It's on Netflix!
+
+  //else print
+  //title, year, imdb rating, country, language, plot, actors, rotten tomatoes, rotten tomatoes url
+}
+
+if(command == 'do-what-it-says'){
+  //get text inside random.txt
+}
